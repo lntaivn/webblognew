8a4788a3b2959d5b52a589f28a0622dfecc09dea
nnsang1309 <nguyennhatsang2002@gmail.com>
2023-12-27 15:57:36 +0700

    fix

--- content/blog.php
+++ content/blog.php
@@ -73,6 +73,7 @@ $comment = $row["number_of_comments"];
       a i {
             color: black;
       }
+
       .wizard-image {
             max-width: 950px !important;
             max-height: 339px;
@@ -101,8 +102,8 @@ $comment = $row["number_of_comments"];
                               </div>
                               <div>
                                     <?php
-                        echo $ranking;
-                        ?>
+                                    echo $ranking;
+                                    ?>
                               </div>
 
                         </li>
@@ -122,75 +123,79 @@ $comment = $row["number_of_comments"];
                         <!-- Đây là phần nơi nội dung bài viết sẽ được hiển thị -->
                         <!-- Khi truy vấn và hiển thị bài viết, bạn có thể gọi hàm showBlog($id_bai_viet) ở đây -->
                         <?php
-                function showBlog($id)
-                {
-                    global $kn;
+                        function showBlog($id)
+                        {
+                              global $kn;
 
-                    $sql = "SELECT blog.*, user.name AS author_name, user.id_user, blog.banner FROM blog JOIN user ON blog.id_user = user.id_user WHERE id_blog = ?";
-                    $stmt = mysqli_prepare($kn, $sql);
-                    mysqli_stmt_bind_param($stmt, "i", $id);
-                    mysqli_stmt_execute($stmt);
-                    $result = mysqli_stmt_get_result($stmt);
+                              $sql = "SELECT blog.*, user.name AS author_name, user.id_user, blog.banner FROM blog JOIN user ON blog.id_user = user.id_user WHERE id_blog = ?";
+                              $stmt = mysqli_prepare($kn, $sql);
+                              mysqli_stmt_bind_param($stmt, "i", $id);
+                              mysqli_stmt_execute($stmt);
+                              $result = mysqli_stmt_get_result($stmt);
 
 
-                    if ($row = mysqli_fetch_assoc($result)) {
-                        // Tạo một đối tượng Parsedown mới
-                        $parsedown = new Parsedown();
+                              if ($row = mysqli_fetch_assoc($result)) {
+                                    // Tạo một đối tượng Parsedown mới
+                                    $parsedown = new Parsedown();
 
-                        // Chuyển đổi Markdown sang HTML
-                        $htmlContent = $parsedown->text($row['content']);
-                        // Display the blog content dynamically
-                        echo '<img src="../' . $row["banner"] . '" alt="Banner Image" class="wizard-image">';
-                        echo '<div class="article-info">';
-                        echo '<div class="blog-ranking">';
-                        echo '</div>';
-                        echo '<div class="author-info">';
-                        echo '<img src="./../img/logo.png" alt="' . htmlspecialchars($row['author_name']) . '" class="author-image">';
-                        echo '<div class="author-details">';
-                        echo '<span class="author-name">' . htmlspecialchars($row['author_name']) . '</span>';
-                        echo '<span class="post-date">Posted on ' . htmlspecialchars($row['date']) . '</span>';
-                        echo '</div></div>';
-                        echo '<h1 class="article-title">' . htmlspecialchars($row['title']) . '</h1>';
-                        echo '<p class="article-summary">' . htmlspecialchars($row['summary']) . '</p>';
-                        echo '<div class="article-content">' . $htmlContent . '</div>';
-                        echo '<div class="tags">';
-                        // Tags or other elements could be added here
-                        echo '</div></div>';
-                    } else {
-                        echo 'Blog post not found.';
-                    }
-                }
+                                    // Chuyển đổi Markdown sang HTML
+                                    $htmlContent = $parsedown->text($row['content']);
+                                    // Display the blog content dynamically
+                                    echo '<img src="../' . $row["banner"] . '" alt="Banner Image" class="wizard-image">';
+                                    echo '<div class="article-info">';
+                                    echo '<div class="blog-ranking">';
+                                    echo '</div>';
+                                    echo '<div class="author-info">';
+                                    echo '<img src="./../img/logo.png" alt="' . htmlspecialchars($row['author_name']) . '" class="author-image">';
+                                    echo '<div class="author-details">';
+                                    echo '<span class="author-name">' . htmlspecialchars($row['author_name']) . '</span>';
+                                    echo '<span class="post-date">Posted on ' . htmlspecialchars($row['date']) . '</span>';
+                                    echo '</div></div>';
+                                    echo '<h1 class="article-title">' . htmlspecialchars($row['title']) . '</h1>';
+                                    echo '<p class="article-summary">' . htmlspecialchars($row['summary']) . '</p>';
+                                    echo '<div class="article-content">' . $htmlContent . '</div>';
+                                    echo '<div class="tags">';
+                                    // Tags or other elements could be added here
+                        
 
-                if ($id_bai_viet) {
-                    showBlog($id_bai_viet);
-                }
-                ?>
+                                    
+
+                                    echo '</div></div>';
+                              } else {
+                                    echo 'Blog post not found.';
+                              }
+                        }
+
+                        if ($id_bai_viet) {
+                              showBlog($id_bai_viet);
+                        }
+                        ?>
 
                   </div>
                   <div class="center-comment">
                         <?php
-                include('../comment/comment.php');
-                ?>
+                        include('../comment/comment.php');
+                        ?>
                   </div>
             </div>
 
             <!-- thong tin nguoi dung -->
             <?php
-        include("../config/dbconfig.php");
-        // Giả sử bạn muốn hiển thị thông tin người dùng có id_user là 1
-        $id_user = 1;
+            include("../config/dbconfig.php");
+            // Giả sử bạn muốn hiển thị thông tin người dùng có id_user là 1
+            $id_user = 1;
 
-        // Truy vấn cơ sở dữ liệu để lấy thông tin người dùng
-        $sql = "SELECT * FROM user WHERE id_user = ?";
-        $stmt = $kn->prepare($sql);
-        $stmt->bind_param("i", $id_user);
-        $stmt->execute();
-        $result = $stmt->get_result();
+            // Truy vấn cơ sở dữ liệu để lấy thông tin người dùng
+            $sql = "SELECT * FROM user WHERE id_user = ?";
+            $stmt = $kn->prepare($sql);
+            $stmt->bind_param("i", $id_user);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            // Lấy thông tin người dùng và hiển thị
-            $row = $result->fetch_assoc();
-            ?>
+            if ($result->num_rows > 0) {
+                  // Lấy thông tin người dùng và hiển thị
+                  $row = $result->fetch_assoc();
+                  ?>
             <aside class="user-profile">
                   <!-- <img src="./img/<?php //echo htmlspecialchars($row['profile_picture']); ?>" alt="Profile Picture" -->
                   <img src="./../img/logo.png" alt="Profile Picture" class="profile-picture">
@@ -213,12 +218,12 @@ $comment = $row["number_of_comments"];
                   </div>
             </aside>
             <?php
-        } else {
-            echo "User not found.";
-        }
-        // Đóng kết nối
-        $kn->close();
-        ?>
+            } else {
+                  echo "User not found.";
+            }
+            // Đóng kết nối
+            $kn->close();
+            ?>
 
       </div>
 
